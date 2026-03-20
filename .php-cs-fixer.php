3f72139e4c5c499dc0c916a9d94733f0615079c2
overtrue <anzhengchao@gmail.com>
2026-03-20 23:43:52 +0800

    chore: Laravel 13 support (PHP 8.3+)

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -29,7 +29,7 @@ return (new PhpCsFixer\Config())
         ],
         'return_type_declaration' => true,
         'short_scalar_cast' => true,
-        'single_blank_line_before_namespace' => true,
+        // single_blank_line_before_namespace removed (conflicts with PSR12)
         'single_trait_insert_per_statement' => true,
         'ternary_operator_spaces' => true,
         'unary_operator_spaces' => true,
